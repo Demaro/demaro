7076c25e4dbab0c5176cdbf8f83c1b8419cedf5f
David Martinez <dmartinez@bancoconsorcio.cl>
2018-12-18 13:15:28 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+
+  scroll(id) {
+
+    console.log('el id: ', id);
+    const el = document.getElementById(id);
+    el.scrollIntoView();
+  }
+}
