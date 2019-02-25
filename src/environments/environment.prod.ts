c30146734e4987d7a1f10c609d87e7500701f1e5
David Martinez <dmartinez@bancoconsorcio.cl>
2019-02-25 18:26:35 -0300

    update with firebase auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAK8o2IHWjvaNL2iM9eLfRi6wyJFGvNeds",
+    authDomain: "demaro-create.firebaseapp.com",
+    databaseURL: "https://demaro-create.firebaseio.com",
+    projectId: "demaro-create",
+    storageBucket: "demaro-create.appspot.com",
+    messagingSenderId: "149230688145"
+  }
